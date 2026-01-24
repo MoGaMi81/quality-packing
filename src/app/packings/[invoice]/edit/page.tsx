a3d66dc18a052645483222dba4dbc8f599337713
Michel Morfín <michel_moga3@hotmail.com>
2026-01-23 19:56:26 -0600

    fix: remove red debug bar

--- src/app/packings/[invoice]/edit/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function EditPacking({ params }: { params: { invoice: string }}) {
-  return (
-    <main className="p-6">
-      <h1 className="text-3xl font-bold">Editar Packing {params.invoice}</h1>
-      <p>Editor en construcción…</p>
-    </main>
-  );
-}
