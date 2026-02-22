b8dc582d8f989befd79f04e65c65f0406d035149
Michel Morfín <michel_moga3@hotmail.com>
2026-02-22 15:24:41 -0600

    fix: remove red debug bar

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -43,7 +43,7 @@ export default function AdminDashboard() {
         <Card
           title="Ver"
           desc="Consulta general de packings"
-          onClick={() => router.push("/packings/${invoice_no}/view")}
+          onClick={() => router.push("/admin/view")}
         />
 
         <Card
